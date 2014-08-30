5c4ac8a4181c74c12e115168b8c8b9b1e7b4be86
scottsievert <sieve121@umn.edu>
2014-08-30 11:19:30 -0500

    animation

--- meta.py
+++ meta.py
@@ -39,33 +39,21 @@ def draw_circle(percentage, image, r=70, center=(90, 148)):
     mask = sector_mask(shape=image.shape, percentage=percentage, radius=r, centre=center)
     image[mask] = 1
     return image
-
 def draw_bars(panel_black, comic, height=400):
     base = 154
     for i in arange(3):
         bar_height = round(height * panel_black[i])
         comic[base-bar_height:base, 300+60*i:300+60*i+30] = 1
-
-    
-panels = 3
-WIDTH = 246
-
-comic = imread('base.png')
-comic = 1 - np.round(comic)
-HEIGHT = comic.shape[0]
-n = comic.shape[0] * comic.shape[1]
-
-
 def draw_figure():
     # updating first panel
-    number_black = argwhere(comic > 0.9).shape[0]
+    number_black = argwhere(comic > BLACK).shape[0]
     percentage = number_black / n
     draw_circle(percentage, comic)
 
     # updating second panel
     panel_black = zeros(3)
     for i in arange(3):
-        panel_black[i] = argwhere(comic[:, i*WIDTH:(i+1)*WIDTH] > 0.9).shape[0]
+        panel_black[i] = argwhere(comic[:, i*WIDTH:(i+1)*WIDTH] > BLACK).shape[0]
         panel_black[i] *= 1 / (WIDTH * HEIGHT)
     draw_bars(panel_black, comic)
 
@@ -77,20 +65,34 @@ def draw_figure():
     base_x, base_y = (80, 530)
     comic[base_x:base_x+rn, base_y:base_y+rm] = resized
 
-# for drawnow
-S = 12
-ion()
-figure(figsize=(S, S/3))
-def update_image():
-    set_cmap('binary')
-    imshow(comic, interpolation='nearest')
-    axis('off')
-    
+if __name__ == "__main__":
+    panels = 3
+    WIDTH = 246
+    BLACK = 0.2
 
-for i in arange(5):
-    draw_figure()
-    drawnow(update_image)
-    sleep(1)
+    comic = imread('base.png')
+    #comic = 1 - np.round(comic)
+    comic = 1 - comic
+    HEIGHT = comic.shape[0]
+    n = comic.shape[0] * comic.shape[1]
+
+
+
+    # for drawnow
+    S = 12
+    ion()
+    figure(figsize=(S, S/3))
+    def update_image():
+        set_cmap('binary')
+        imshow(comic)#, interpolation='nearest')
+        axis('off')
+        title('Iteration %d' % (i+1))
+        
+
+    for i in arange(7):
+        draw_figure()
+        drawnow(update_image)
+        sleep(1)
 
 
 
--- readme.md
+++ readme.md
@@ -3,6 +3,8 @@
 
 ![xkcd comic](self_description.png)
 
+![animation](animation.gif)
+
 
 XKCD has an interesting plot in [#688]. They do a several plots that include
 information about the plot in the comic. At first, this seems hard to
@@ -12,5 +14,8 @@ have to use a binary search or use some fancy method.
 But no, since the image is correct and gradually converges to some value, you
 just call the function over and over again.
 
+This seems basic, but this is a complex mathematical problem. I'm guessing that
+there's no closed form solution and if there was, it'd be almost impossible to
+write down.
 
 [#688]:https://xkcd.com/688/
